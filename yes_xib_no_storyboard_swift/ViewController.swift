4c15b9492b6aa274d9fea3a43bb7a8ac4c0e1dee
麻生 拓弥 <MILANISTA@Milanista-rMBP.local>
2015-05-24 16:49:48 +0900

    Initial Commit

---
+++ yes_xib_no_storyboard_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yes_xib_no_storyboard_swift
+//
+//  Created by 麻生 拓弥 on 2015/05/24.
+//  Copyright (c) 2015年 麻生 拓弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
